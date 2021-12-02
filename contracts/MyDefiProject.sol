84c4ade9048d758621f1971a0df6d9b2bc47d777
IlyaPrigogine <prigogine@protonmail.com>
2021-12-03 06:38:59 +0800

    added singleSwap func in MyDefiProject.sol

--- contracts/MyDefiProject.sol
+++ contracts/MyDefiProject.sol
@@ -7,7 +7,32 @@ import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract MyDefiProject {
     ISwapRouter public immutable swapRouter;
+
+    address public constant DAI = 0xaD6D458402F60fD3Bd25163575031ACDce07538D; //dai in ropsten
+    address public constant WETH9 = 0xc778417E063141139Fce010982780140Aa0cD5Ab; //weth in ropsten
+    uint24 public constant poolFee = 3000;
+
     constructor (ISwapRouter _swapRouter) {
         swapRouter = _swapRouter;
+    }
+
+    function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
+        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
+        TransferHelper.safeApprove(DAI,address (swapRouter), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn : DAI,
+            tokenOut : WETH9,
+            fee : poolFee,
+            recipient : msg.sender,
+            deadline : block.timestamp,
+            amountIn : amountIn,
+            amountOutMinimum : 0,
+            sqrtPriceLimitX96 : 0
+        });
+
+        amountOut = swapRouter.exactInputSingle(params);
+        return amountOut;
+
     }
 }
--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -1,10 +1,14 @@
 import {deployments, ethers, getNamedAccounts} from 'hardhat';
+import {MyDefiProject} from "../typechain";
 
 const {execute, read} = deployments;
 
 async function main() {
 
     const {owner} = await getNamedAccounts();
+
+    const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
+    console.log(`${await MyDefiProject.address}`);
 
 
 }
