d3d72402df4204ba8f2827d39044c20e3cc12175
IlyaPrigogine <prigogine@protonmail.com>
2021-12-03 06:49:02 +0800

    submitted

--- Helpers/constants.ts
+++ Helpers/constants.ts
@@ -1,2 +1,5 @@
 export const swapAddress_ropsten = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
-export const swapAmount = '1.987654321'
+export const dai_ropsten = '0xaD6D458402F60fD3Bd25163575031ACDce07538D';
+export const weth_ropsten = '0xc778417E063141139Fce010982780140Aa0cD5Ab';
+export const swapAmount = '3.54321'
+export const initialApproveAmount = '10000'; //10_000;
--- scripts/dai_funcs.ts
+++ scripts/dai_funcs.ts
@@ -1,5 +1,7 @@
-import {deployments, ethers, getNamedAccounts} from 'hardhat';
-import {MyDefiProject} from "../typechain";
+import {deployments, ethers, getNamedAccounts, network} from 'hardhat';
+import {IERC20, MyDefiProject} from "../typechain";
+import {dai_ropsten, initialApproveAmount, swapAmount, weth_ropsten} from "../Helpers/constants";
+import {formatEther, parseEther} from "ethers/lib/utils";
 
 const {execute, read} = deployments;
 
@@ -7,8 +9,19 @@ async function main() {
 
     const {owner} = await getNamedAccounts();
 
-    const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
-    console.log(`${await MyDefiProject.address}`);
+    if (await network.name === 'ropsten') {
+        const MyDefiProject = await ethers.getContract<MyDefiProject>('MyDefiProject');
+        // console.log(`${await MyDefiProject.address}`);
+
+        // const dai = await ethers.getContractAt<IERC20>('IERC20',dai_ropsten);
+        // await dai.approve(MyDefiProject.address,parseEther(initialApproveAmount));
+        //
+        // const weth = await ethers.getContractAt<IERC20>('IERC20',weth_ropsten);
+        // await weth.approve(MyDefiProject.address,parseEther(initialApproveAmount));
+
+        await MyDefiProject.swapExactInputSingle(parseEther(swapAmount));
+    }
+
 
 
 }
